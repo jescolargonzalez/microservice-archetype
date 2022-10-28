50e917c458fa47e5aa3d5e2eb29724900a71364a
FaceKey <jesusescolargonzalez@gmail.com>
2022-10-28 20:55:23 +0200

    Initial commit with spring initializer structure

---
+++ src/main/java/com/insurance/archetype/microservicearchetype/MicroserviceArchetypeApplication.java
@@ -0,0 +1,13 @@
+package com.insurance.archetype.microservicearchetype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceArchetypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceArchetypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insurance/archetype/microservicearchetype/MicroserviceArchetypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.archetype.microservicearchetype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceArchetypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
